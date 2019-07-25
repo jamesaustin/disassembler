375d92c0c41e7cb93d0e5489c6420431d62e7b84
James Austin <jamesaustin@mac.com>
2019-07-25 12:12:43 +0100

    Tidy with: black -l 120

--- disassembler.py
+++ disassembler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 from __future__ import print_function
 
 from os.path import join as path_join
@@ -9,58 +9,71 @@ from itertools import chain
 from sys import stdin as sys_stdin
 
 import logging
+
 LOG = logging.getLogger(__name__)
 
-PROCESS_CHOICES = ['compact', 'pretty', 'debug', 'keys']
+PROCESS_CHOICES = ["compact", "pretty", "debug", "keys"]
 DEFAULT_INDENTATION = 2
 DEFAULT_DEPTH = 5
 DEFAULT_DICT_ELEMENTS = 4
 DEFAULT_LIST_ELEMENTS = 2
-DEFAULT_PATH = '*'
+DEFAULT_PATH = "*"
 
 PYTHON_TYPES = False
-TRUE_STR, FALSE_STR, NULL_STR = ('True', 'False', 'None') if PYTHON_TYPES else ('true', 'false', 'none')
+TRUE_STR, FALSE_STR, NULL_STR = ("True", "False", "None") if PYTHON_TYPES else ("true", "false", "none")
 
 TEST_NUM_ELEMENTS = 10
+
+
 def _test_json():
     def k(k):
-        return 'k{}'.format(k)
-    return json_loads(json_dumps({
-        'listEmpty': [],
-        'listInt': list(range(TEST_NUM_ELEMENTS)),
-        'listFloat': [1.0 / (x + 1) for x in range(TEST_NUM_ELEMENTS)],
-        'listStr': [str(x) for x in range(TEST_NUM_ELEMENTS)],
-        'listBool': [x % 2 == 0 for x in range(TEST_NUM_ELEMENTS)],
-        'listNull': [None for x in range(TEST_NUM_ELEMENTS)],
-        'listDict': [{k(x): x} for x in range(TEST_NUM_ELEMENTS)],
-        'listList': [list(range(10)) for x in range(TEST_NUM_ELEMENTS)],
-        'listListListList': [[[[None]]]],
-        'dictEmpty': {},
-        'dictInt': {k(x): x for x in range(TEST_NUM_ELEMENTS)},
-        'dictUnicode': {u'k{}'.format(x): u'{}'.format(x) for x in range(TEST_NUM_ELEMENTS)},
-        'dictStr': {k(x): str(x) for x in range(TEST_NUM_ELEMENTS)},
-        'dictDict': {k(x): {k(x): x} for x in range(TEST_NUM_ELEMENTS)},
-        'dictDictDictDict': {'a': {'b': {'c': {'d': 'e'}}}},
-        'int': 1,
-        'float': 1.0,
-        'unicode': u'hello',
-        'str': 'hello',
-        'boolTrue': True,
-        'boolFalse': False,
-        'null': None
-    }))
+        return "k{}".format(k)
+
+    return json_loads(
+        json_dumps(
+            {
+                "listEmpty": [],
+                "listInt": list(range(TEST_NUM_ELEMENTS)),
+                "listFloat": [1.0 / (x + 1) for x in range(TEST_NUM_ELEMENTS)],
+                "listStr": [str(x) for x in range(TEST_NUM_ELEMENTS)],
+                "listBool": [x % 2 == 0 for x in range(TEST_NUM_ELEMENTS)],
+                "listNull": [None for x in range(TEST_NUM_ELEMENTS)],
+                "listDict": [{k(x): x} for x in range(TEST_NUM_ELEMENTS)],
+                "listList": [list(range(10)) for x in range(TEST_NUM_ELEMENTS)],
+                "listListListList": [[[[None]]]],
+                "dictEmpty": {},
+                "dictInt": {k(x): x for x in range(TEST_NUM_ELEMENTS)},
+                "dictUnicode": {u"k{}".format(x): u"{}".format(x) for x in range(TEST_NUM_ELEMENTS)},
+                "dictStr": {k(x): str(x) for x in range(TEST_NUM_ELEMENTS)},
+                "dictDict": {k(x): {k(x): x} for x in range(TEST_NUM_ELEMENTS)},
+                "dictDictDictDict": {"a": {"b": {"c": {"d": "e"}}}},
+                "int": 1,
+                "float": 1.0,
+                "unicode": u"hello",
+                "str": "hello",
+                "boolTrue": True,
+                "boolFalse": False,
+                "null": None,
+            }
+        )
+    )
+
 
 def json_debug(j, args):
     output = []
+
     def _output(*parts):
-        output.append(''.join(parts))
+        output.append("".join(parts))
+
     def _indent(depth):
-        return ' ' * (depth * args.indent)
+        return " " * (depth * args.indent)
 
     def _dict_test(items):
         return args.all or args.dict == 0 or len(items) <= (args.dict * 2)
+
     def _list_test(items):
         return args.all or args.list == 0 or len(items) <= (args.list * 2)
+
     def _depth_test(depth):
         return args.all or args.depth == 0 or depth < args.depth
 
@@ -78,6 +91,7 @@ def json_debug(j, args):
                         if fnmatch(_path, args.path) or _child_hit(_path, v):
                             return True
                 return False
+
             return not args.path or fnmatch(this_path, args.path) or _child_hit(this_path, child)
 
         if isinstance(j, dict):
@@ -88,8 +102,9 @@ def json_debug(j, args):
                     if _path_hit(this_path, j[k]):
                         yield n, k, j[k], this_path
             else:
-                for n, k in chain(enumerate(keys[:args.dict]),
-                                  enumerate(keys[-args.dict:], start=len(keys) - args.dict)):
+                for n, k in chain(
+                    enumerate(keys[: args.dict]), enumerate(keys[-args.dict :], start=len(keys) - args.dict)
+                ):
                     yield n, k, j[k], path_join(path, k)
         elif isinstance(j, list):
             if args.path or _list_test(j):
@@ -98,10 +113,10 @@ def json_debug(j, args):
                     if _path_hit(this_path, v):
                         yield n, None, v, this_path
             else:
-                for n, v in chain(enumerate(j[:args.list]), enumerate(j[-args.list:], start=len(j) - args.list)):
+                for n, v in chain(enumerate(j[: args.list]), enumerate(j[-args.list :], start=len(j) - args.list)):
                     yield n, None, v, path_join(path, str(n))
         else:
-            LOG.error('Unsupported type: %s', type(j))
+            LOG.error("Unsupported type: %s", type(j))
             exit()
 
     def _item(j):
@@ -110,33 +125,33 @@ def json_debug(j, args):
         elif isinstance(j, bool):
             return TRUE_STR if j else FALSE_STR
         elif isinstance(j, (float, int)):
-            return '{}'.format(j)
+            return "{}".format(j)
         else:
             return u'"{}"'.format(j)
 
     def _info(path, count=0, value=None):
         if not args.paths and not args.counts:
-            return ''
+            return ""
 
         if args.paths:
             key_str = path
         else:
-            key_str = ''
+            key_str = ""
 
         if args.counts:
             if value is None:
-                count_str = ''
+                count_str = ""
             elif count == len(value):
-                count_str = '<{}>'.format(count)
+                count_str = "<{}>".format(count)
             else:
-                count_str = '<{} of {}>'.format(count, len(value))
+                count_str = "<{} of {}>".format(count, len(value))
         else:
-            count_str = ''
+            count_str = ""
 
         if not key_str and not count_str:
-            return ''
+            return ""
 
-        return u' # {}{}'.format(key_str, count_str)
+        return u" # {}{}".format(key_str, count_str)
 
     def _basic_list(j, path):
         basic = []
@@ -145,35 +160,35 @@ def json_debug(j, args):
         for n, _, v, _ in _culled(j, path):
             rendered += 1
             if n > 0 and n != previous_n + 1:
-                basic.append('... ')
+                basic.append("... ")
             previous_n = n
             basic.append(_item(v))
-        _output(', '.join(basic))
+        _output(", ".join(basic))
         return rendered
 
-    def _recurse(j, depth=0, path='/'):
+    def _recurse(j, depth=0, path="/"):
         if isinstance(j, dict):
             if len(j) == 0:
-                _output('{}', _info(path, 0, j))
+                _output("{}", _info(path, 0, j))
                 return
             elif not _depth_test(depth):
-                _output('{ ... }', _info(path, 0, j))
+                _output("{ ... }", _info(path, 0, j))
                 return
             else:
-                _output('{\n')
+                _output("{\n")
         elif isinstance(j, list):
             if len(j) == 0:
-                _output('[]', _info(path, 0, j))
+                _output("[]", _info(path, 0, j))
                 return
             elif not _depth_test(depth):
-                _output('[ ... ]', _info(path, 0, j))
+                _output("[ ... ]", _info(path, 0, j))
                 return
             elif any(isinstance(x, (dict, list)) for x in j):
-                _output('[\n')
+                _output("[\n")
             else:
-                _output('[')
+                _output("[")
                 rendered = _basic_list(j, path)
-                _output(']', _info(path, rendered, j))
+                _output("]", _info(path, rendered, j))
                 return
         else:
             _output(_item(j))
@@ -189,32 +204,34 @@ def json_debug(j, args):
 
             # Is there a gap in the output, if so add a '...'
             if n > 0 and n != previous_n + 1:
-                _output(indent, '...\n')
+                _output(indent, "...\n")
             previous_n = n
 
             if isinstance(v, dict):
                 _output(prefix)
                 _recurse(v, depth, this_path)
-                _output('\n')
+                _output("\n")
             elif isinstance(v, list):
                 _output(prefix)
                 _recurse(v, depth, this_path)
-                _output('\n')
+                _output("\n")
             else:
-                _output(prefix, _item(v), _info(this_path), '\n')
+                _output(prefix, _item(v), _info(this_path), "\n")
 
         indent = _indent(depth - 1)
         if isinstance(j, dict):
-            _output(indent, '}', _info(path, rendered, j))
+            _output(indent, "}", _info(path, rendered, j))
         elif isinstance(j, list):
-            _output(indent, ']', _info(path, rendered, j))
+            _output(indent, "]", _info(path, rendered, j))
 
     _recurse(j)
     return output
 
+
 def keys_debug(j, args):
     output = []
-    def _recurse(j, depth=0, path='/'):
+
+    def _recurse(j, depth=0, path="/"):
         # TODO add support for "dict" + "list"?
         # TODO add support for "path"
         if args.all or args.depth == 0 or depth < args.depth:
@@ -228,26 +245,28 @@ def keys_debug(j, args):
             elif isinstance(j, list):
                 for n, v in enumerate(j):
                     _recurse(v, depth, path_join(path, str(n)))
+
     _recurse(j)
     return output
 
+
 def parse_args():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('input', nargs='*', type=FileType('r'), default=[sys_stdin], help='JSON files to process')
-    parser.add_argument('--style', choices=PROCESS_CHOICES, default='debug', help='Style of JSON output')
-    group = parser.add_argument_group('what to output?')
-    group.add_argument('--all', action='store_true', help='Output everything')
-    group.add_argument('--indent', type=int, default=DEFAULT_INDENTATION, help='Print indentation')
-    group.add_argument('--depth', type=int, default=DEFAULT_DEPTH, help='Depth to recurse')
-    group.add_argument('--dict', type=int, default=DEFAULT_DICT_ELEMENTS, help='Num dict entries to display')
-    group.add_argument('--list', type=int, default=DEFAULT_LIST_ELEMENTS, help='Num list entries to display')
-    group.add_argument('--paths', action='store_true', help='Output item paths')
-    group.add_argument('--counts', action='store_true', help='Output item counts')
-    group.add_argument('--path', type=str, help='File pattern selection of elements to display')
-    group = parser.add_argument_group('debugging options')
-    group.add_argument('--verbose', '-v', action='store_true')
-    group.add_argument('--debug', action='store_true')
-    group.add_argument('--test', action='store_true', help='Test the tool against a built-in unit test')
+    parser.add_argument("input", nargs="*", type=FileType("r"), default=[sys_stdin], help="JSON files to process")
+    parser.add_argument("--style", choices=PROCESS_CHOICES, default="debug", help="Style of JSON output")
+    group = parser.add_argument_group("what to output?")
+    group.add_argument("--all", action="store_true", help="Output everything")
+    group.add_argument("--indent", type=int, default=DEFAULT_INDENTATION, help="Print indentation")
+    group.add_argument("--depth", type=int, default=DEFAULT_DEPTH, help="Depth to recurse")
+    group.add_argument("--dict", type=int, default=DEFAULT_DICT_ELEMENTS, help="Num dict entries to display")
+    group.add_argument("--list", type=int, default=DEFAULT_LIST_ELEMENTS, help="Num list entries to display")
+    group.add_argument("--paths", action="store_true", help="Output item paths")
+    group.add_argument("--counts", action="store_true", help="Output item counts")
+    group.add_argument("--path", type=str, help="File pattern selection of elements to display")
+    group = parser.add_argument_group("debugging options")
+    group.add_argument("--verbose", "-v", action="store_true")
+    group.add_argument("--debug", action="store_true")
+    group.add_argument("--test", action="store_true", help="Test the tool against a built-in unit test")
     args = parser.parse_args()
 
     if args.debug:
@@ -256,23 +275,24 @@ def parse_args():
         level = logging.INFO
     else:
         level = logging.WARNING
-    logging.basicConfig(format='%(message)s', level=level)
-    LOG.debug('# %s', args)
+    logging.basicConfig(format="%(message)s", level=level)
+    LOG.debug("# %s", args)
 
     return args
+
 
 def main():
     args = parse_args()
 
     def _process(j):
-        if args.style == 'compact':
-            print(json_dumps(j, sort_keys=True, separators=(',', ':')))
-        elif args.style == 'pretty':
-            print(json_dumps(j, sort_keys=True, indent=args.indent, separators=(',', ': ')))
-        elif args.style == 'keys':
-            print('\n'.join(keys_debug(j, args)))
-        elif args.style == 'debug':
-            print(''.join(json_debug(j, args)))
+        if args.style == "compact":
+            print(json_dumps(j, sort_keys=True, separators=(",", ":")))
+        elif args.style == "pretty":
+            print(json_dumps(j, sort_keys=True, indent=args.indent, separators=(",", ": ")))
+        elif args.style == "keys":
+            print("\n".join(keys_debug(j, args)))
+        elif args.style == "debug":
+            print("".join(json_debug(j, args)))
 
     if args.test:
         _process(_test_json())
@@ -282,5 +302,6 @@ def main():
         j = json_load(f)
         _process(j)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
